3ccd102b92894f4a3daecad6a885d3fe1dc936fa
AdamKruk91 <adam.kruk91@gmail.com>
2018-06-27 23:46:49 +0200

    Add interfaces for pojo and class

---
+++ src/main/java/com/tanksDevs/system/entity/water/Water.java
@@ -0,0 +1,8 @@
+package com.tanksDevs.system.entity.water;
+
+import com.tanksDevs.system.entity.Colliding;
+
+public interface Water extends Colliding {
+
+    //marker
+}
---
+++ src/main/java/com/tanksDevs/system/entity/water/WaterPojo.java
@@ -0,0 +1,8 @@
+package com.tanksDevs.system.entity.water;
+
+import com.tanksDevs.system.pojo.EntityPojo;
+
+public interface WaterPojo extends EntityPojo {
+
+    //marker
+}
