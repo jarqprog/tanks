95a68be9eec96e4892a1335047e2a9f96b9d09a7
AdamKruk91 <adam.kruk91@gmail.com>
2018-06-27 13:29:32 +0200

    Add AbstractEntity class with getters and setters

---
+++ src/main/java/com/tanksDevs/system/entity/AbstractEntity.java
@@ -0,0 +1,39 @@
+package com.tanksDevs.system.entity;
+
+public abstract class AbstractEntity implements Entity{
+    private int id;
+    private int x;
+    private int y;
+    private int size;
+
+    public AbstractEntity(int id) {
+        this.id = id;
+    }
+
+    public AbstractEntity(int id, int x, int y, int size) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+}
