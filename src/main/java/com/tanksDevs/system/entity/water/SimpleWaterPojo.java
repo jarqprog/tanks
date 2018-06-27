dee9c6d372d17dac047ab88636846c599d02ed32
AdamKruk91 <adam.kruk91@gmail.com>
2018-06-27 23:47:26 +0200

    Add SimpleWater, SimpleWaterPojo implementation

---
+++ src/main/java/com/tanksDevs/system/entity/water/SimpleWaterPojo.java
@@ -0,0 +1,6 @@
+package com.tanksDevs.system.entity.water;
+
+import com.tanksDevs.system.pojo.AbstractEntityPojo;
+
+public class SimpleWaterPojo extends AbstractEntityPojo implements WaterPojo {
+}
