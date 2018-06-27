8d802c453cf1e4b919c64b7af7e20a77599234de
AdamKruk91 <adam.kruk91@gmail.com>
2018-06-27 20:19:15 +0200

    Create TankBasePojo interface

---
+++ src/main/java/com/tanksDevs/system/entity/eagle/TankBasePojo.java
@@ -0,0 +1,8 @@
+package com.tanksDevs.system.entity.eagle;
+
+import com.tanksDevs.system.pojo.EntityPojo;
+
+public interface TankBasePojo extends EntityPojo {
+    int getHp();
+    void setHp(int hp);
+}
