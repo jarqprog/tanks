808d41ccab69ddc197d3f8837beae298434b8a89
Michal Gruszka <m.gruszka215@gmail.com>
2018-06-28 00:13:29 +0200

    Add getters to HitBox

--- src/main/java/com/tanksDevs/system/entity/hitBox/HitBox.java
+++ src/main/java/com/tanksDevs/system/entity/hitBox/HitBox.java
@@ -3,4 +3,8 @@ package com.tanksDevs.system.entity.hitBox;
 public interface HitBox {
 
     boolean checkCollision(HitBox other);
+    double getBottomX();
+    double getBottomY();
+    double getTopX();
+    double getTopY();
 }
