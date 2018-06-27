90afe93275d2bfd3bac0c0620b7582f6b8dcc445
AdamKruk91 <adam.kruk91@gmail.com>
2018-06-27 14:37:23 +0200

    Add TankBase interface

---
+++ src/main/java/com/tanksDevs/system/entity/eagle/TankBase.java
@@ -0,0 +1,8 @@
+package com.tanksDevs.system.entity.eagle;
+
+import com.tanksDevs.system.entity.Colliding;
+import com.tanksDevs.system.entity.Destructible;
+
+public interface TankBase extends Colliding, Destructible {
+
+}
