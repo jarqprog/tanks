fae520ff6d298aba5cde0b27c787e4c027c7df65
AdamKruk91 <adam.kruk91@gmail.com>
2018-06-27 11:42:31 +0200

    Add User constructor, getters and setters

---
+++ src/main/java/com/tanksDevs/system/player/User.java
@@ -0,0 +1,51 @@
+package com.tanksDevs.system.player;
+
+import com.tanksDevs.system.gameObject.tank.Tank;
+
+public class User implements Player{
+    private Tank tank;
+    private String name;
+    private int score;
+    private boolean isReady;
+
+    public User(String name) { // Todo creating tanks
+        this.name = name;
+        score = 0;
+        isReady = false;
+    }
+
+    @Override
+    public Tank getTank() {
+        return tank;
+    }
+
+    @Override
+    public void setTank(Tank tank) {
+        this.tank = tank;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public boolean isReady() {
+        return isReady;
+    }
+
+    @Override
+    public void setReady(boolean ready) {
+        isReady = ready;
+    }
+}
