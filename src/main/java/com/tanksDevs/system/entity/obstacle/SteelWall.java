9c1f7868e531c9e795b3cf4a02080161ef195f6f
AdamKruk91 <adam.kruk91@gmail.com>
2018-06-27 13:53:44 +0200

    Add SteelWall class with Interface implementation

--- src/main/java/com/tanksDevs/system/entity/obstacle/SteelWall.java
+++ src/main/java/com/tanksDevs/system/entity/obstacle/SteelWall.java
@@ -1,4 +1,22 @@
 package com.tanksDevs.system.entity.obstacle;
 
-public class SteelWall {
+import com.tanksDevs.system.entity.AbstractEntity;
+import com.tanksDevs.system.entity.Colliding;
+import com.tanksDevs.system.entity.hitBox.HitBox;
+
+public class SteelWall extends AbstractEntity implements Colliding {
+
+    public SteelWall(int id, int x, int y, int size) {
+        super(id, x, y, size);
+    }
+
+    @Override
+    public boolean isCollision(Colliding other) { //Todo implement collision
+        return false;
+    }
+
+    @Override
+    public HitBox getHitBox() {
+        return null;
+    }
 }
