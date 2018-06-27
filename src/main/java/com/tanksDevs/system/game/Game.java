6062a55fd248a25e0174d808e893be3cf15c06d4
jarqprog <jarqprog@gmail.com>
2018-06-27 17:45:01 +0200

    add methods to Game interface

--- src/main/java/com/tanksDevs/system/game/Game.java
+++ src/main/java/com/tanksDevs/system/game/Game.java
@@ -1,4 +1,20 @@
 package com.tanksDevs.system.game;
 
+import com.tanksDevs.system.board.Board;
+import com.tanksDevs.system.entity.Colliding;
+import com.tanksDevs.system.entity.tank.Tank;
+import com.tanksDevs.system.player.Player;
+
+import java.util.Set;
+
 public interface Game {
+
+    Board getBoard();
+    Set<Colliding> getColliding();
+    Set<Player> getPlayers();
+    Set<Tank> getTanks();
+    boolean markPlayerTank(Tank tank);
+    boolean registerPlayer(Player player);
+    // todo potrzeba wymyślić co z bazą!
+
 }
