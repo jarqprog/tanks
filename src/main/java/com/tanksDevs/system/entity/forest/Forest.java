2f228da2f6df09d398f2aa33739f4ad667ed94d6
AdamKruk91 <adam.kruk91@gmail.com>
2018-06-27 15:08:10 +0200

    Delete implementing Colliding interface in Forest

--- src/main/java/com/tanksDevs/system/entity/forest/Forest.java
+++ src/main/java/com/tanksDevs/system/entity/forest/Forest.java
@@ -1,7 +1,5 @@
 package com.tanksDevs.system.entity.forest;
 
-import com.tanksDevs.system.entity.Colliding;
-
-public interface Forest extends Colliding {
+public interface Forest {
 
 }
--- src/main/java/com/tanksDevs/system/entity/forest/SimpleForest.java
+++ src/main/java/com/tanksDevs/system/entity/forest/SimpleForest.java
@@ -1,22 +1,10 @@
 package com.tanksDevs.system.entity.forest;
 
 import com.tanksDevs.system.entity.AbstractEntity;
-import com.tanksDevs.system.entity.Colliding;
-import com.tanksDevs.system.entity.hitBox.HitBox;
 
 public class SimpleForest extends AbstractEntity implements Forest {
 
     public SimpleForest(int id, int x, int y, int size) {
         super(id, x, y, size);
-    }
-
-    @Override
-    public boolean isCollision(Colliding other) { //Todo collision handling
-        return false;
-    }
-
-    @Override
-    public HitBox getHitBox() {
-        return null;
     }
 }
