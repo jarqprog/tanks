3da462365255417cfdd60edfa88eaececf891d40
AdamKruk91 <adam.kruk91@gmail.com>
2018-06-27 11:41:23 +0200

    Add methods for interface Player

--- src/main/java/com/tanksDevs/system/player/Player.java
+++ src/main/java/com/tanksDevs/system/player/Player.java
@@ -1,4 +1,13 @@
 package com.tanksDevs.system.player;
 
+import com.tanksDevs.system.gameObject.tank.Tank;
+
 public interface Player {
+    Tank getTank();
+    void setTank(Tank tank);
+    int getScore();
+    void setScore(int score);
+    String getName();
+    boolean isReady();
+    void setReady(boolean ready);
 }
