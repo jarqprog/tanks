72c713c37ec26700476a34176fc583560ee7097c
AdamKruk91 <adam.kruk91@gmail.com>
2018-06-28 14:25:33 +0200

    Add speed and direction in TankPojo

--- src/main/java/com/tanksDevs/system/entity/tank/SimpleTankPojo.java
+++ src/main/java/com/tanksDevs/system/entity/tank/SimpleTankPojo.java
@@ -1,13 +1,36 @@
 package com.tanksDevs.system.entity.tank;
 
+import com.tanksDevs.system.entity.Direction;
 import com.tanksDevs.system.pojo.AbstractEntityPojo;
 
 public class SimpleTankPojo extends AbstractEntityPojo implements TankPojo {
 
+    private double speed;
+    private Direction direction;
     private int hp;
     private boolean isOccupied;
 
     public SimpleTankPojo() {}
+
+    @Override
+    public Direction getDirection() {
+        return direction;
+    }
+
+    @Override
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    @Override
+    public double getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
 
     @Override
     public int getHp() {
--- src/main/java/com/tanksDevs/system/entity/tank/TankPojo.java
+++ src/main/java/com/tanksDevs/system/entity/tank/TankPojo.java
@@ -1,9 +1,14 @@
 package com.tanksDevs.system.entity.tank;
 
+import com.tanksDevs.system.entity.Direction;
 import com.tanksDevs.system.pojo.EntityPojo;
 
 public interface TankPojo extends EntityPojo {
 
+    Direction getDirection();
+    void setDirection(Direction direction);
+    double getSpeed();
+    void  setSpeed(double speed);
     int getHp();
     void setHp(int hp);
     boolean getIsOccupied();
