5ce5efad2bb4ebcc0b75b4e00d1d6ea7820a5299
Jacek <jacek.ragann@gmail.com>
2018-06-28 17:36:07 +0200

    Create Main menu with working buttons (except editor)

---
+++ src/main/java/com/tanksDevs/layout/mainmenu/MainMenu.java
@@ -0,0 +1,36 @@
+package com.tanksDevs.layout.mainmenu;
+
+import com.tanksDevs.App;
+import com.tanksDevs.layout.Screen;
+import com.tanksDevs.layout.utils.Graphics;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+
+public class MainMenu extends Screen {
+
+    private App app;
+
+    public MainMenu(App app) {
+        this.app = app;
+        setRoot(buildRoot());
+    }
+
+    @Override
+    protected Parent buildRoot() {
+        StackPane root = new StackPane();
+        GridPane menu = new CentralMenu().build(root, app);
+        ImageView titleImage = new ImageView(TITLE);
+
+        menu.setAlignment(Pos.CENTER);
+        StackPane imageContainer = new StackPane(titleImage);
+        imageContainer.setAlignment(Pos.TOP_CENTER);
+
+        root.getChildren().add(imageContainer);
+        root.getChildren().add(menu);
+        root.setBackground(Graphics.getBackground(BACKGROUND));
+        return root;
+    }
+}
